4517d5b4230112b0134c433b2f75abbc72d7e261
Tran Duc Anh <tran.duc.anh@framgia.com>
2018-02-12 09:27:58 +0700

    Finish: sample blockchain app

---
+++ blockchain_python.py
@@ -0,0 +1,38 @@
+import hashlib as hasher
+import datetime as date
+
+class Block:
+  def __init__(self, index, timestamp, data, previous_hash):
+    self.index = index
+    self.timestamp = timestamp
+    self.data = data
+    self.previous_hash = previous_hash
+    self.hash = self.hash_block()
+
+  def hash_block(self):
+    sha = hasher.sha256()
+    sha.update(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash))
+    return sha.hexdigest()
+
+def create_genesis_block():
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
+
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "Hello world! I'm block" + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)
+
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+num_of_blocks_to_add = 20
+
+for i in range(0, num_of_blocks_to_add):
+  block_to_add = next_block(previous_block)
+  blockchain.append(block_to_add)
+  previous_block = block_to_add
+
+  print "Block #{} da duoc them vao blockchain".format(block_to_add.index)
+print "Hash: {}\n".format(block_to_add.hash)
